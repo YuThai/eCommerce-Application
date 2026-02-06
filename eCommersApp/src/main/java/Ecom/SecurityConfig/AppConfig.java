239c0cca0cbc06a11cbc5ed1414d0321067a8d37
YuThai <yu382005@gmail.com>
2026-02-06 18:24:00 +0700

    Fix AuthenticationManager to use ProviderManager with custom AuthenticationProvider

--- eCommersApp/src/main/java/Ecom/SecurityConfig/AppConfig.java
+++ eCommersApp/src/main/java/Ecom/SecurityConfig/AppConfig.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -132,8 +132,8 @@ public class AppConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
+    public AuthenticationManager authenticationManager() throws Exception {
+        return new ProviderManager(authenticationProvider());
     }
 }
 
