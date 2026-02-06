5a60ae3de68d75a81d2d410489e5c240849f9c1f
YuThai <yu382005@gmail.com>
2026-02-06 16:04:21 +0700

    Add missing AuthenticationManager bean to AppConfig

--- eCommersApp/src/main/java/Ecom/SecurityConfig/AppConfig.java
+++ eCommersApp/src/main/java/Ecom/SecurityConfig/AppConfig.java
@@ -6,6 +6,8 @@ import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -108,9 +110,11 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-
     }
 
-
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
 
